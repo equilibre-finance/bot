933eaa155c0b373e8b623b0a8b17de4fe4d3fbee
wendel <19378023+bitdeep@users.noreply.github.com>
2022-12-11 13:21:54 -0300

    modifications to switch from OP to KAVA

--- src/constants/tokenIds.ts
+++ src/constants/tokenIds.ts
@@ -3,5 +3,5 @@
 export const TOKENS: { [key: string]: (string | number)[] } = {
   '0xbfb032e2a9564e50e970c6aef4c1e664d1f98d36': ['eq-token', 'EQ', 18, 'velo.png'],
   '0x66cd011adff20f2b4ba60cdd30099b5e09ccacd1': ['equilibre-finance', 'VARA', 18, 'velo.png'],
-  '0xf4ac9d42f0dd250be21a1465716700a95dd18d63': ['usd-coin', 'USDC', 6, '6319/small/USD_Coin_icon.png'],
+  '0xf4ac9d42f0dd250be21a1465716700a95dd18d63': ['usd-coin', 'USDC', 6, 'USD_Coin_icon.png'],
 }
