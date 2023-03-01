9f5979c2bae5cbc7a43581ebf32f4c2807cb532f
bitdeep <19378023+bitdeep@users.noreply.github.com>
2023-03-01 13:03:16 -0300

    gc token id

--- app/src/constants/tokenIds.ts
+++ app/src/constants/tokenIds.ts
@@ -1,7 +1,7 @@
 // [COINGECKO ID, SYMBOL, DECIMALS, CONGECKO-LOGO]
 
 export const TOKENS: { [key: string]: (string | number)[] } = {
-  '0xe1da44c0da55b075ae8e2e4b6986adc76ac77d73': ['equilibre-finance', 'VARA', 18, 'https://i.imgur.com/kVO6weI.png'],
+  '0xe1da44c0da55b075ae8e2e4b6986adc76ac77d73': ['equilibre', 'VARA', 18, 'https://i.imgur.com/kVO6weI.png'],
   '0xfa9343c3897324496a05fc75abed6bac29f8a40f': ['usd-coin', 'USDC', 6, 'https://assets.coingecko.com/coins/images/6319/large/USD_Coin_icon.png'],
   '0xc86c7c0efbd6a49b35e8714c5f59d99de09a225b': ['kava', 'WKAVA', 18, 'https://i.imgur.com/oQHfzWV.png'],
   '0x765277eebeca2e31912c9946eae1021199b39c61': ['dai', 'DAI', 18, 'https://assets.coingecko.com/coins/images/9956/large/4943.png?1636636734'],
